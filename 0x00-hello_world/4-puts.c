7765490a8841c76b6070d353540cb87a50297a0c
Abdulazee224 <lewisdoptimist@gmail.com>
2022-10-13 14:27:31 -0700

    c program that prints a function with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * return:'0' if succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
