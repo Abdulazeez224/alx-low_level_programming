461c7ab847f3c2d951fc206ef84d69f6fdc17d05
Abdulazee224 <lewisdoptimist@gmail.com>
2022-10-14 04:22:53 -0700

    C program that use puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,7 @@
  * Description: The return value of main is to indicate how the program exited
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
