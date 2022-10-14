b384cb8a1b71f9919d79f51dd9ac33b847f7887e
Abdulazee224 <lewisdoptimist@gmail.com>
2022-10-14 04:12:33 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints a sequence of characters
- * return:0 if successful
+ * main - Entry point
+ * @void: Specify no argument
+ *
+ * Description: The return value of main is to indicate how the program exited
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
