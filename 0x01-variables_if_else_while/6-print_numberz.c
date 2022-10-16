bc5d9a7622c276ff4efc2e51368fb47c40019247
Abdulazee224 <lewisdoptimist@gmail.com>
2022-10-16 12:13:01 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (successful)
+  */
+
+int main(void)
+{
+	int alph;
+	
+	for (alph = 0; alph <= 9; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
