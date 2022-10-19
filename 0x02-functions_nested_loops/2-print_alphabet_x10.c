b2075c3e35f57634b5a99f0f1a0ca68c3fdf1bd6
Abdulazee224 <lewisdoptimist@gmail.com>
2022-10-18 20:00:20 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - Entry point
+  */
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	count = 0;
+
+	while (count < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
